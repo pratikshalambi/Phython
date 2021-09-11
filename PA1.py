7ecaeebdaa1c2410cd9b8dd7c2320cd22c663d41
pratikshalambi <88720527+pratikshalambi@users.noreply.github.com>
2021-09-11 12:45:20 +0530

    Add files via upload
    Programming Assignment_1

---
+++ PA1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1
+print("hello python")
+
+
+# In[28]:
+
+
+#2
+a=input("enter first number: ")
+b=input("enter second number: ")
+c=float(a)
+d=float(b)
+print("Addition:",c+d)
+print("Division:",c/d)
+
+
+# In[26]:
+
+
+#3
+a=input("Give the base of the Triangle: ")
+b=input("Give the height of the Triangle: ")
+c=float(a)
+d=float(b)
+e=0.5*(c*d)
+print("Area of Triangle is:",e)
+
+
+# In[18]:
+
+
+#4
+x = input("Give the value of x :")
+y = input ("Give the value o y :")
+x, y = y, x
+print("x =", x)
+print("y =", y)
+
+
+# In[25]:
+
+
+#5
+import random
+print(random.randint(0,90))
+
+
+# In[ ]:
+
+
+
+
